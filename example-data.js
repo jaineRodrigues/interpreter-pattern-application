8431af8554537b33e8fb33fe2516ce4243ceb6fd
jaineRodrigues <jainer469@gmail.com>
2023-11-07 17:35:19 -0300

    fixing comments

--- example-data.js
+++ example-data.js
@@ -1,75 +1,88 @@
-import readline from 'readline';
+import readline from "readline";
 
+// Classe Context: Mantém o contexto da data e da expressão
 class Context {
-  constructor(date) {
-    this.date = date;
-    this.expression = "";
-  }
+    constructor(date) {
+        this.date = date; // Data atual
+        this.expression = ""; // Expressão da data a ser formatada
+    }
 }
 
+// Classe IAbstractExpression: Interface para expressões abstratas.  // Método para avaliar a expressão
 class IAbstractExpression {
-  evaluate(context) {}
+    evaluate(context) {}
 }
 
+// Classe ExpressaoDia: Implementa a expressão para o dia. // Substitui "DD" na expressão pela parte do dia da data
 class ExpressaoDia extends IAbstractExpression {
-  evaluate(context) {
-    context.expression = context.expression.replace("DD", context.date.getDate());
-  }
+    evaluate(context) {
+        context.expression = context.expression.replace("DD", context.date.getDate());
+    }
 }
 
+// Classe ExpressaoMes: Implementa a expressão para o mês.   // Substitui "MM" na expressão pela parte do mês da data (adicionando +1 porque os meses começam em 0)
 class ExpressaoMes extends IAbstractExpression {
-  evaluate(context) {
-    context.expression = context.expression.replace("MM", context.date.getMonth() + 1);
-  }
+    evaluate(context) {
+        context.expression = context.expression.replace("MM", context.date.getMonth() + 1);
+    }
 }
 
 class ExpressaoAno extends IAbstractExpression {
-  evaluate(context) {
-    context.expression = context.expression.replace("YYYY", context.date.getFullYear());
-  }
+    evaluate(context) {
+        context.expression = context.expression.replace("YYYY", context.date.getFullYear());
+    }
 }
 
+// Classe Separador: Implementa a expressão para substituir espaços por hífens
 class Separador extends IAbstractExpression {
-  evaluate(context) {
-    context.expression = context.expression.replace(" ", "-");
-  }
+    evaluate(context) {
+        context.expression = context.expression.replace(" ", "-");
+    }
 }
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
+// Função principal
 async function main() {
-  const expression = await new Promise((resolve) => {
-    rl.question("Selecione a expressão a usar: MM-DD-YYYY ou YYYY-MM-DD ou DD-MM-YYYY: ", (expr) => resolve(expr));
-  });
+    const expression = await new Promise((resolve) => {
+        rl.question("Selecione a expressão a usar: MM-DD-YYYY ou YYYY-MM-DD ou DD-MM-YYYY: ", (expr) => resolve(expr));
+    });
 
-  const date = new Date();
-  const context = new Context(date);
-  context.expression = expression.toUpperCase();
+    // Obtém a data atual
+    const date = new Date();
 
-  const formatParts = context.expression.split('-');
+    // Cria um contexto com a data atual e a expressão fornecida pelo usuário
+    const context = new Context(date);
+    context.expression = expression.toUpperCase();
 
-  const expressions = [];
-  for (const part of formatParts) {
-    if (part === "DD") {
-      expressions.push(new ExpressaoDia());
-    } else if (part === "MM") {
-      expressions.push(new ExpressaoMes());
-    } else if (part === "YYYY") {
-      expressions.push(new ExpressaoAno());
+    // Divide a expressão em partes com base nos hífens
+    const formatParts = context.expression.split("-");
+
+    // Cria uma lista de expressões com base nas partes da expressão
+    const expressions = [];
+    for (const part of formatParts) {
+        if (part === "DD") {
+            expressions.push(new ExpressaoDia());
+        } else if (part === "MM") {
+            expressions.push(new ExpressaoMes());
+        } else if (part === "YYYY") {
+            expressions.push(new ExpressaoAno());
+        }
     }
-  }
 
-  expressions.push(new Separador());
+    expressions.push(new Separador());
 
-  for (const expr of expressions) {
-    expr.evaluate(context);
-  }
+    for (const expr of expressions) {
+        expr.evaluate(context);
+    }
 
-  console.log(`Data na expressão escolhida: ${context.expression}`);
-  rl.close();
+    // Exibe a data formatada com base na expressão
+    console.log(`Data na expressão escolhida: ${context.expression}`);
+    rl.close();
 }
 
+// Chama a função principal para iniciar o programa
 main();
--- interpreter-example.js
+++ interpreter-example.js
@@ -5,18 +5,14 @@ class Context {
         this.query = query;
     }
 
-    // Método para iniciar a busca
     search() {
-        // Cria um objeto QueryParser para analisar a consulta
         const parser = new QueryParser(this.query);
-        // Chama o método parse do QueryParser para buscar os livros correspondentes
         return parser.parse(this.books);
     }
 }
 
 // AbstractExpression - Interface para as expressões
 class AbstractExpression {
-    // Método interpret para avaliar a expressão em um livro
     interpret(book) {}
 }
 
@@ -28,7 +24,6 @@ class TerminalExpression extends AbstractExpression {
         this.value = value;
     }
 
-    // Avalia se a propriedade do livro é igual ao valor fornecido
     interpret(book) {
         return book[this.property] === this.value;
     }
@@ -43,7 +38,6 @@ class NonterminalExpression extends AbstractExpression {
         this.operator = operator;
     }
 
-    // Avalia as duas expressões combinadas com base no operador (AND ou OR)
     interpret(book) {
         if (this.operator === "AND") {
             return this.expression1.interpret(book) && this.expression2.interpret(book);
@@ -60,45 +54,35 @@ class QueryParser {
         this.query = query;
     }
 
-    // Método para analisar a consulta e buscar livros correspondentes
     parse(books) {
         const tokens = this.query.split(" ");
         const expressionStack = [];
         let operator = null;
 
-        // Analisa a consulta token por token
         tokens.forEach((token) => {
             if (token === "AND" || token === "OR") {
-                // Se o token for um operador, armazena o operador
                 operator = token;
             } else {
-                // Se o token for uma propriedade:valor, cria uma expressão Terminal
                 const [property, value] = token.split(":");
                 const expression = new TerminalExpression(property, value);
                 if (operator) {
-                    // Se houver um operador pendente, combina as expressões anteriores
                     const expression2 = expressionStack.pop();
                     const expression1 = expressionStack.pop();
                     expressionStack.push(new NonterminalExpression(expression1, expression2, operator));
                     operator = null;
                 }
-                // Empilha a expressão atual
                 expressionStack.push(expression);
             }
         });
 
-        // Após analisar a consulta, a árvore de expressões é construída
         if (expressionStack.length === 1) {
-            // Se houver apenas uma expressão na pilha, avalia a consulta
             return books.filter((book) => expressionStack[0].interpret(book));
         } else {
-            // Caso contrário, retorna um array vazio
             return [];
         }
     }
 }
 
-// Exemplo de uso
 // Dados de exemplo
 const books = [
     { title: "The Hobbit", author: "Tolkien", genre: "Fantasy" },
@@ -106,14 +90,14 @@ const books = [
     { title: "Harry Potter", author: "Rowling", genre: "Fantasy" }
 ];
 
-// Função para buscar livros com base na consulta (não faz parte do padrão Interpreter)
+// Função para buscar livros com base na consulta
 function searchBooks(books, query) {
     const [property, value] = query.split(":");
     return books.filter((book) => book[property] === value);
 }
 
-// Exemplo de uso da consulta com a função
-const query = "author:Tolkien" || "genre:Fantasy";
+// Exemplo de uso
+const query = "author:Tolkien";
 const results = searchBooks(books, query);
 
 console.log(results);
